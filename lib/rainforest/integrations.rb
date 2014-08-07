b8d43c24ba5fc448aa5adbb020d125558ebd9226
Simon Mathieu <simon.math@gmail.com>
2014-08-06 17:14:59 -0700

    Initial commit

---
+++ lib/rainforest/integrations.rb
@@ -0,0 +1,7 @@
+require "rainforest/integrations/version"
+
+module Rainforest
+  module Integrations
+    # Your code goes here...
+  end
+end
---
+++ lib/rainforest/integrations/version.rb
@@ -0,0 +1,5 @@
+module Rainforest
+  module Integrations
+    VERSION = "0.0.1"
+  end
+end
