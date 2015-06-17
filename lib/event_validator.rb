440f46cd60545c623029ed488166917eb50be2d8
Emanuel Evans <=>
2015-06-17 15:50:49 -0700

    Move events data into data folder

--- lib/event_validator.rb
+++ lib/event_validator.rb
@@ -1,5 +1,5 @@
 class EventValidator
-  EVENTS = YAML.load File.read(Rails.root.join('config', 'events.yml')).freeze
+  EVENTS = YAML.load File.read(Rails.root.join('data', 'events.yml')).freeze
 
   class InvalidPayloadError < StandardError
   end
@@ -10,11 +10,11 @@ class EventValidator
   end
 
   def validate!
-    keys = EVENTS.fetch(@event_name.to_s) do
+    event = EVENTS.fetch(@event_name.to_s) do
       raise InvalidPayloadError, "Event #{@event_name} is not supported"
     end
 
-    keys.map! &:to_sym
+    keys = event.fetch('required_keys').map(&:to_sym)
     unless keys & @payload.keys == keys
       raise InvalidPayloadError, "Payload for event #{@event_name} did not contain required keys #{keys.map(&:to_s)}"
     end
